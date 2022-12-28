95086652b903ac93d8a530d677aa9bd72c7fa528
Mukurohoshi <110019140+Mukurohoshi@users.noreply.github.com>
2022-12-28 23:00:41 +0100

    add dossier + index

---
+++ Web-Developer-P6-master/Backend/index.js
@@ -0,0 +1,48 @@
+const http = require("http"); /* programme qui attend les requetes http et qui y répond */ 
+const app = require('./app'); /* import du fichier app.js */ 
+
+const normalizePort = val => {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+}; /* function qui renvoie un port valide, qu'il soit fourni sous la forme d'un numéro ou d'une chaîne */
+const port = normalizePort(process.env.PORT ||'3000');
+app.set('port', port); /* configuration pour dire à l'application express sur quel port elle doit tourner */
+
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges.');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use.');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}; /* function qui recherche les différentes erreurs et les gère de manière appropriée. Elle est ensuite enregistrée dans le serveur ;*/
+
+const server = http.createServer(app); /* Création du server a partir du fichier app.js*/
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind);
+});
+
+
+server.listen(port); /* le serveur écoute le port */ 
